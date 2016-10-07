21b06f2fd33bf6b47bb55b4a3ad42b69d1b4cafa
zhouyichen <zhouyichen123@gmail.com>
2016-10-07 23:09:33 +0800

    extract homography matrices for all training data

---
+++ common.py
@@ -0,0 +1,6 @@
+key_points = [36, 39, 27, 42, 45, 48, 51, 54, 8]
+
+NUMBER_OF_POSES = 8
+NUMBER_OF_LANDMARK_PER_FACE = len(key_points)
+LANDMAKRS_DATA = "landmarks.csv"
+HOMOGRAPHY_DATA = "homography.csv"
