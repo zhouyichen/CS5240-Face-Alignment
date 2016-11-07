c4cb657a4463b8c6ee0b0ce7b5c7110cd1cccf2d
zhouyichen <zhouyichen123@gmail.com>
2016-11-07 12:21:35 +0800

    clean up unused files

--- common.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-key_points = [36, 39, 42, 45, 48, 54, 8]
-
-NUMBER_OF_POSES = 8
-NUMBER_OF_LANDMARK_PER_FACE = len(key_points)
-NUMBER_LM_PRE = str(NUMBER_OF_LANDMARK_PER_FACE)
-LANDMAKRS_DATA = "landmarks.csv"
-HOMOGRAPHY_DATA = "homography.csv"
